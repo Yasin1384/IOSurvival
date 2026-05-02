8ce0783f870cfe5651d6fbda02d206a75534039c
yasin <yasinamb.1384amb.11228@gmail.com>
2026-05-02 15:00:02 +0330

    add navmesh for path finding

--- Assets/Script/Moves/MoveEnemy/MovementEnemy.cs
+++ Assets/Script/Moves/MoveEnemy/MovementEnemy.cs
@@ -1,5 +1,6 @@
 using Unity.Burst.CompilerServices;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MovementEnemy : MonoBehaviour
 {
@@ -8,26 +9,14 @@ public class MovementEnemy : MonoBehaviour
     private Transform player;
     private EnemyPool enemyPool;
     private EnemyType_SO enemyType_SO;
-
-    private PathFinding obstacleManager;
-    public float avoidStrength;
-
-    [Header("Avoidance")]
-    public float detectDistanceMultiplier;
-    public float sphereRadius;
     
     private Vector3 moveDir;
 
-    public LayerMask obstacleMask;
-
-    private Vector3 lockedDir;
-    private float lockTime = 0f;
-    public float lockDuration;
+    public NavMeshAgent NavMeshAgentAi;
 
     void Start()
     {
         GameObject playerObj = GameObject.FindGameObjectWithTag(playerTag);
-        obstacleManager = FindObjectOfType<PathFinding>();
         if (playerObj != null)
             player = playerObj.transform;
 
@@ -36,74 +25,20 @@ public class MovementEnemy : MonoBehaviour
 
     void Update()
     {
-        if (player == null || enemyType_SO == null) return;
-
-        MoveToPlayer();
+        SpeedSnemies(enemyType_SO);
     }
-    private void MoveToPlayer()
+
+    public void SpeedSnemies(EnemyType_SO enemyType_SO)
     {
-        float speed = enemyType_SO.Speed;
+        this.enemyType_SO = enemyType_SO;
 
-        Vector3 origin = transform.position + Vector3.up * 0.5f;
-
-        Vector3 toPlayer = (player.position - transform.position);
-        toPlayer.y = 0;
-        toPlayer.Normalize();
-
-        Vector3 forward = toPlayer;
-
-        Vector3 desiredDir = forward;
-
-        float detectDistance = speed * 1f;
-
-        if (lockTime > 0)
+        if (player != null)
         {
-            lockTime -= Time.deltaTime;
-            desiredDir = lockedDir;
+            Vector3 targetPlayer = new Vector3(player.position.x, transform.position.y, player.position.z);
+
+            NavMeshAgentAi.SetDestination(targetPlayer);
         }
-        else
-        {
-            bool blocked = Physics.SphereCast(origin, sphereRadius, forward, out RaycastHit hit, detectDistance, obstacleMask);
-
-            if (blocked)
-            {
-                Vector3 left = Quaternion.Euler(0, -45, 0) * forward;
-                Vector3 right = Quaternion.Euler(0, 45, 0) * forward;
-
-                bool leftBlocked = Physics.SphereCast(origin, sphereRadius, left, out RaycastHit hit1, detectDistance, obstacleMask);
-                bool rightBlocked = Physics.SphereCast(origin, sphereRadius, right, out RaycastHit hit2, detectDistance, obstacleMask);
-
-                if (!leftBlocked)
-                {
-                    desiredDir = left;
-                }
-                else if (!rightBlocked)
-                {
-                    desiredDir = right;
-                }
-                else
-                {
-                    desiredDir = -forward;
-                }
-
-                lockedDir = desiredDir;
-                lockTime = lockDuration;
-            }
-        }
-
-        moveDir = Vector3.Lerp(moveDir, desiredDir, 1f * Time.deltaTime);
-
-        transform.position += moveDir * speed * Time.deltaTime;
-
-        transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
-
     }
-
-    public void SetEnemyType(EnemyType_SO type)
-    {
-        enemyType_SO = type;
-    }
-
     public void Die()
     {
         enemyPool.Despawn(gameObject);
--- Assets/Script/PathFinding/PathFinding.cs
+++ Assets/Script/PathFinding/PathFinding.cs
@@ -1,40 +1,40 @@
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class PathFinding : MonoBehaviour
-{
-    public List<GameObject> obstacles;
+//public class PathFinding : MonoBehaviour
+//{
+//    public List<GameObject> obstacles;
 
-    private List<Bounds> obstacleBounds = new List<Bounds>();
+//    private List<Bounds> obstacleBounds = new List<Bounds>();
 
-    void Start()
-    {
-        UpdateBounds();
-    }
+//    void Start()
+//    {
+//        UpdateBounds();
+//    }
 
-    public void UpdateBounds()
-    {
-        obstacleBounds.Clear();
+//    public void UpdateBounds()
+//    {
+//        obstacleBounds.Clear();
 
-        foreach (var obj in obstacles)
-        {
-            if (obj == null) continue;
+//        foreach (var obj in obstacles)
+//        {
+//            if (obj == null) continue;
 
-            Collider col = obj.GetComponent<Collider>();
-            if (col != null)
-            {
-                obstacleBounds.Add(col.bounds);
-            }
-        }
-    }
+//            Collider col = obj.GetComponent<Collider>();
+//            if (col != null)
+//            {
+//                obstacleBounds.Add(col.bounds);
+//            }
+//        }
+//    }
 
-    public bool IsPositionBlocked(Vector3 position)
-    {
-        foreach (var b in obstacleBounds)
-        {
-            if (b.Contains(position))
-                return true;
-        }
-        return false;
-    }
-}
+//    public bool IsPositionBlocked(Vector3 position)
+//    {
+//        foreach (var b in obstacleBounds)
+//        {
+//            if (b.Contains(position))
+//                return true;
+//        }
+//        return false;
+//    }
+//}

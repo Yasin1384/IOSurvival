6ba8757e89739f7736307d819026e3dae12cd92c
yasin <yasinamb.1384amb.11228@gmail.com>
2026-08-05 22:15:01 +0330

    add feature matchmaking

---
+++ Assets/Script/UI/Popup/Popup_Checknternet/Popup_CheckInternet.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popup_CheckInternet : Popup
+{
+    [SerializeField] private Button checkInternetButton;
+
+
+    public override void Setup()
+    {
+        
+    }
+}
